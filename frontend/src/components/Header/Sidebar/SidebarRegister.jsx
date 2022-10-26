415401cd9d6d1cc2865e2b25874ff9524d18726f
Vinicius Hayden <vinihaydenss@gmail.com>
2022-10-26 00:38:04 -0300

    package-lock.json

---
+++ frontend/src/components/Header/Sidebar/SidebarRegister.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { BsXLg } from "react-icons/bs";
+
+export default function SidebarRegister({ handleChangeState }) {
+  return (
+    <>
+    <div className="sidebar">
+      <Row>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Col>
+          <button type="button" onClick={handleChangeState} className="sidebar-close-button">
+            <BsXLg size={20}></BsXLg>
+          </button>
+        </Col>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Col>
+          <Link to="/login">
+            <input type="button" value="Iniciar sessão" className="sidebar-button" />
+          </Link>
+        </Col>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Col>
+          <Link to="/">
+            <input type="button" value="Menu" className="sidebar-button" />
+          </Link>
+        </Col>
+        </div>
+      </Row>
+    </div>
+    </>
+  );
+}
