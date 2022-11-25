866f523ff9cb44ed62fc41f6ee629778d3804642
Jailson Junior <99207185+JaiJuni0r@users.noreply.github.com>
2022-11-24 23:17:13 -0300

    Create Client.java

---
+++ src/main/java/br/com/dh/emprescar/model/Client.java
@@ -0,0 +1,14 @@
+package br.com.dh.emprescar.model;
+
+public class Client extends User{
+
+    public Client() {
+    }
+
+    public Client(Integer id, String name, String lastName, String email, String password) {
+        super(id, name, lastName, email, password);
+    }
+
+    
+
+}
